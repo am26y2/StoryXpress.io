27b9f92cfb3faab84f6339b13e775048b7338fe4
am26y2 <amitraj9046@gmail.com>
2020-11-23 12:53:58 +0530

    change

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
 import axios from "axios";
+import YearPicker from "react-year-picker";
 
 export default function App() {
-  const [start, setStart] = useState(new Date());
+  const [start, setStart] = useState(2016);
   const apiurl = `localhost:4000`;
   useEffect(() => {
-    async function getData(){
-      const res=await axios.get(apiurl);
+    async function getData() {
+      const res = await axios.get(apiurl);
       console.log(res);
     }
     getData();
   }, []);
+  const handleChange = (el) => {
+    console.log(el);
+    setStart(el);
+  };
   return (
-    <div className="App">
+    <div className="center">
       <div className="row">
-
+        <YearPicker onChange={handleChange} value={start} />
+        <YearPicker onChange={handleChange} value={start} />
       </div>
-      <button>submit</button>
+      <div>
+        <button>submit</button>
+      </div>
     </div>
   );
 }
